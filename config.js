976ca7698ef76fdfde27e182f463447c6257c4f8
hostitorrent <host.itorrent@gmail.com>
2022-03-15 11:05:34 +0100

    main

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: 'JL limitless', // unique addon name
+  prefix: 'jl_ll', // unique addon prefix (must be different for all addons!)
+  icon: 'https://lless2.surge.sh/logo.png', // set the correct domain name here so the logo is accessible
+  description: 'iptv ', // addon description
+  }
